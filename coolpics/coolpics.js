8ed2a649e4f591b951731ca8ddf4a84bc594083e
serich17 <samerich16@byui.edu>
2024-10-11 19:00:43 -0600

    Coolpics update

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,6 +1,6 @@
 const button = document.getElementById("mobile-button"); 
 const list = document.getElementById("links");
-
+document.getElementById("gallery").addEventListener("click", viewHandler)
 
 
 
@@ -14,6 +14,44 @@ function openMenu() {
     }
 }
 
+function handleResize() {
+    const menu = document.querySelector(".mobile-button")
+    if (window.innerWidth > 1000) {
+        menu.classList.remove("hide")
+    }
+    else {
+        menu.classList.add("hide");
+    }
+}
 
+function viewerTemplate(pic, alt) {
+    return `<div class="viewer">
+      <button class="close-viewer" id="close-viewer">X</button>
+      <img src="${pic}" alt="${alt}">
+      </div>`;
+  }
+
+function viewHandler(event) {
+    const picture = event.target
+    console.log(picture)
+    if (event.target.tagName == "IMG") {
+        let source = ((picture.src).split("-"))[0]
+        const imgFile = source + "-full.jpeg"
+
+        const htmlString = viewerTemplate(imgFile, "Full image")
+        const body = document.getElementsByTagName("body")[0]
+        body.insertAdjacentHTML("afterbegin", htmlString)
+        document.getElementById("close-viewer").addEventListener("click", closeViewer)
+    }
+}
+
+function closeViewer() {
+    document.getElementsByClassName("viewer")[0].remove()
+}
+
+
+
+handleResize()
+window.addEventListener("resize", handleResize)
 
 
